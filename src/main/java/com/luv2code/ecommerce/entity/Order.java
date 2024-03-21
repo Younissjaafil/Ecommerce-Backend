b3b19b0de8d7fc33833bfa21b5f776403e556e03
younissjaafil <yjaafeel@gmail.com>
2024-03-21 22:23:43 +0200

    Finish Checkout Form - Save the Order To Backend

--- src/main/java/com/luv2code/ecommerce/entity/Order.java
+++ src/main/java/com/luv2code/ecommerce/entity/Order.java
@@ -16,58 +16,65 @@ import java.util.Set;
 @Getter
 @Setter
 public class Order {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id")
+  @Column(name="id")
   private Long id;
 
-  @Column(name = "order_tracking_number")
+  @Column(name="order_tracking_number")
   private String orderTrackingNumber;
 
-  @Column(name = "total_price")
-  private BigDecimal totalPrice;
-
-  @Column(name = "total_quantity")
+  @Column(name="total_quantity")
   private int totalQuantity;
 
-  @Column(name = "status")
+  @Column(name="total_price")
+  private BigDecimal totalPrice;
+
+  @Column(name="status")
   private String status;
 
-  @Column(name = "date_created")
+  @Column(name="date_created")
   @CreationTimestamp
   private Date dateCreated;
 
-  @Column(name = "last_updated")
+  @Column(name="last_updated")
   @UpdateTimestamp
   private Date lastUpdated;
 
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "order")
   private Set<OrderItem> orderItems = new HashSet<>();
 
-
   @ManyToOne
   @JoinColumn(name = "customer_id")
   private Customer customer;
 
   @OneToOne(cascade = CascadeType.ALL)
-  @JoinColumn(name = "shipping_address_id" , referencedColumnName = "id")
+  @JoinColumn(name = "shipping_address_id", referencedColumnName = "id")
   private Address shippingAddress;
 
   @OneToOne(cascade = CascadeType.ALL)
-  @JoinColumn(name = "billing_address_id" , referencedColumnName = "id")
+  @JoinColumn(name = "billing_address_id", referencedColumnName = "id")
   private Address billingAddress;
 
+  public void add(OrderItem item) {
 
-
-  public void add(OrderItem item){
-    if(item != null){
-      if(orderItems == null){
+    if (item != null) {
+      if (orderItems == null) {
         orderItems = new HashSet<>();
       }
+
       orderItems.add(item);
       item.setOrder(this);
     }
   }
-
-
 }
+
+
+
+
+
+
+
+
+
